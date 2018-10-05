b075479b113c3c6044c56a636559ec16500b8cca
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-10-05 03:21:20 -0400

    card: refactor component

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,10 @@ storiesOf('Card', module).add('Example product card', () => (
     title="Penn Course Review"
     subtitle="https://penncoursereview.com"
     body="The student-run Penn Course Review has served as a valuable guide for course selection since the 1960s. In 2014, Penn Course Review was completely redesigned to simplify the search experience. In 2018, we hope to continue providing you with the best insights on courses and have therefore updated this experience."
-    image="https://pennlabs.org/img/PCR.png"
+    image={{
+      url: 'https://pennlabs.org/img/PCR.png',
+      alt: 'Cool description',
+    }}
   />
 ))
 
