19d16133c03a3a311bea8da94bd5ebfdf05dca11
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-10-05 03:18:55 -0400

    button: remove component

--- src/Button.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import 'bulma/css/bulma.css'
-
-const Button = (props) => {
-    return <button className="button"> {props.text} </button>
-}
-
-export default Button;
--- stories/index.js
+++ stories/index.js
@@ -1,32 +1,17 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Button from '../src/Button';
-import Card from '../src/Card';
-import LoginForm from '../src/LoginForm';
-import SignupForm from '../src/SignupForm';
-import Hero from '../src/Hero';
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Card from '../src/Card'
+import Hero from '../src/Hero'
 
-storiesOf('Button', module)
-  .add('with text', () => <Button text="Penn Labs"/>)
-  .add('with some emoji', () => <Button text="Welcome to Penn Labs 👨‍💻"/>);
-
-storiesOf('Card', module).add('Example product card', () =>
+storiesOf('Card', module).add('Example product card', () => (
   <Card
     title="Penn Course Review"
     subtitle="https://penncoursereview.com"
     body="The student-run Penn Course Review has served as a valuable guide for course selection since the 1960s. In 2014, Penn Course Review was completely redesigned to simplify the search experience. In 2018, we hope to continue providing you with the best insights on courses and have therefore updated this experience."
     image="https://pennlabs.org/img/PCR.png"
   />
-);
+))
 
-storiesOf('LoginForm', module).add("Example", () =>
-  <LoginForm />
-);
-
-storiesOf('SignupForm', module).add("Example", () =>
-  <SignupForm />
-);
-
-storiesOf('Hero', module).add("Example", () =>
-  <Hero title="Penn Labs" subtitle="Penn Labs empowers others to make connections: connections to resources, connections to people, and connections to the greater Penn community."/>
-);
+storiesOf('Hero', module).add('Example', () => (
+  <Hero title="Penn Labs" subtitle="Penn Labs empowers others to make connections: connections to resources, connections to people, and connections to the greater Penn community." />
+))
